c3cdddf7dd11de71bff4761ab68497a15ad8e39c
victorio nogueira <victorionogueira@gmail.com>
2024-10-15 15:05:24 -0300

    add Seller

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,14 +1,20 @@
 package application;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ParseException {
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 		
 		Department obj = new Department(1, "Marketing");
-		System.out.println(obj);
-
+		
+		Seller seller = new Seller(1, "Carlos", "Vic@gamil.com", sdf.parse("04/03/2004"), 10000.0, obj);
+		System.out.println(seller);
 	}
 
 }

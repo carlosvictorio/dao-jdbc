01824eb6f282ceed70c95d4cef561c9b53b53276
victorio nogueira <victorionogueira@gmail.com>
2024-10-17 16:58:19 -0300

    findById Implemetation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,19 +1,18 @@
 package application;
 
 import java.text.ParseException;
-import java.text.SimpleDateFormat;
 
-import model.entities.Department;
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) throws ParseException {
-		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+
+		SellerDao sellerDao = DaoFactory.createSellerDao();
+		Seller seller = sellerDao.findById(8);
 		
-		Department obj = new Department(1, "Marketing");
-		
-		Seller seller = new Seller(1, "Carlos", "Vic@gamil.com", sdf.parse("04/03/2004"), 10000.0, obj);
 		System.out.println(seller);
 	}
 

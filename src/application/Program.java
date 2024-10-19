d2419665962ec990e12e00a056eb60343d29b2e6
victorio nogueira <victorionogueira@gmail.com>
2024-10-19 14:37:07 -0300

    FindaById

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,11 @@
 package application;
 
 import java.text.ParseException;
+import java.util.List;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
+import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
@@ -15,6 +17,12 @@ public class Program {
 		System.out.println("---- Teste 1: Find By ID ----");
 		Seller seller = sellerDao.findById(8);
 		System.out.println(seller);
+		System.out.println("\n----Teste 2: seller findByDepartment ----");
+		Department department = new Department(2, null);
+		List<Seller> list = sellerDao.findByDepartment(department);
+		for (Seller obj : list) {
+			System.out.println(obj);
+		}
 	}
 
 }
